3b44842f83d01f83e2056df29a19c0ba32d1f763
Bart Salle <info@bartsalle.nl>
2018-11-12 18:34:48 +0100

    Changed classname

--- plugin.php
+++ plugin.php
@@ -1,5 +1,5 @@
 <?php
-    class pluginHello extends Plugin {
+    class pluginBackendChanges extends Plugin {
         public function adminHead() {
             $html .= $this->includeCSS('changes.css');
             return $html;
